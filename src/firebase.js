8ccf3415d62e42b921a241e58e93e2932495b769
manyyeon <udayeon@naver.com>
2022-02-03 21:30:58 +0900

    ss

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAewloLFjfZjrSKf7DGQ9Nu4IPA9ShxDk0",
-  authDomain: "nwitter4.firebaseapp.com",
-  projectId: "nwitter4",
-  storageBucket: "nwitter4.appspot.com",
-  messagingSenderId: "664505659658",
-  appId: "1:664505659658:web:7349a04bf2eff320108af3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
