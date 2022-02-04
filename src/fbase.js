b1c5606e934f4b863395deb3ed0692ca66c4f6e9
manyyeon <udayeon@naver.com>
2022-02-04 19:20:44 +0900

    2.0 Using Firebase Auth

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
